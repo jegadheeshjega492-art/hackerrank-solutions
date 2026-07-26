5032323d985d66483da1321cf51361b7c9b5b846
https://github.com/jegadheeshjega492-art/portfolio <jegadheeshjega492@gmail.com>
2026-07-26 14:39:59 +0530

    Added new solutions

---
+++ count the students socred marks.py
@@ -0,0 +1,12 @@
+students = {
+    "Ram" : 86,
+    "Sam" : 90,
+    "Hari" : 78,
+    "Jez" : 89,
+    "Powsi" : 99
+}
+count = 0
+for key in students:
+    if students[key]:
+        count+=1
+print(count)        
